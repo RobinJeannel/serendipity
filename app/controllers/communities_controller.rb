60936735d148cb015e12a7810c77e7b831279b7d
RobinJeannel <rjeanneldethiersant@gmail.com>
2014-08-26 18:52:57 +0200

    devise done

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,2 +1,16 @@
 class CommunitiesController < ApplicationController
+
+  def index
+    @communities = Community.all
+  end
+
+  def show
+    @Community = Community.find(params[:id])
+  end
+
+  private
+  def community_params
+    params.require(:community).permit(:name)
+  end
 end
+
