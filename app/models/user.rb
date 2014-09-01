de16fd0d74d6ba4da13e6ff7192320e37f42a25c
RobinJeannel <rjeanneldethiersant@gmail.com>
2014-09-01 10:44:03 +0200

    fb connect only done, changes is user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
         user.provider = auth.provider
         user.uid = auth.uid
         user.email = auth.info.email
-        user.password = Devise.friendly_token[0,20]
+        # user.password = Devise.friendly_token[0,20]
         user.name = auth.info.name   # assuming the user model has a name
         user.picture = auth.info.image # assuming the user model has an image
         user.token = auth.credentials.token
